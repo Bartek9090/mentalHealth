7ae4f93154071bb2d4a1e9d64cd8f11f84ea21c9
Bartek Bartosik <b.bartek987@gmail.com>
2023-08-22 23:52:47 +0200

    turn of menu a

--- script.js
+++ script.js
@@ -1,59 +1,67 @@
 // Navbar
 const menu = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar");
+const navLinks = document.querySelectorAll(".nav-link");
 
 menu.addEventListener("click", () => {
   navbar.classList.toggle("change");
-  menu.classList.toggle("change")
+  menu.classList.toggle("change");
 });
+
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    navbar.classList.toggle("change");
+  });
+});
+
 // End of Navbar
 
 // Section 2 Video
-const video = document.querySelector('.video')
-const btn = document.querySelector('.buttons button i');
-const bar = document.querySelector('.video-bar')
+const video = document.querySelector(".video");
+const btn = document.querySelector(".buttons button i");
+const bar = document.querySelector(".video-bar");
 
 const playPause = () => {
-  if(video.paused) {
-    video.play()
-    btn.className = 'far fa-pause-circle'
-    video.style.opacity = '.7'
+  if (video.paused) {
+    video.play();
+    btn.className = "far fa-pause-circle";
+    video.style.opacity = ".7";
   } else {
-    video.pause()
-    btn.className = 'far fa-play-circle'
-    video.style.opacity = '.3'
+    video.pause();
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
   }
-}
+};
 
-btn.addEventListener('click', () => {
-  playPause()
-})
+btn.addEventListener("click", () => {
+  playPause();
+});
 
-
-video.addEventListener('timeupdate', () => {
-  console.log(video.currentTime, video.duration)
+video.addEventListener("timeupdate", () => {
+  console.log(video.currentTime, video.duration);
   const barWidth = video.currentTime / video.duration;
-  bar.style.width = `${barWidth * 100}%` 
-  if(video.ended) {
-    btn.className = 'far fa-play-circle'
-    video.style.opacity = '.3'
-  } 
-})
+  bar.style.width = `${barWidth * 100}%`;
+  if (video.ended) {
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
+  }
+});
 // End of Section 2 Video
 
-var swiper = new Swiper('.swiper-container', {
-  effect: 'coverflow',
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   coverflowEffect: {
     rotate: 70,
     stretch: 0,
     depth: 100,
     modifier: 1,
     slideShadows: true,
-  }
+  },
 });
 
-
-{/* <video class="video" src="img/sweden.mp4"></video> */}
+{
+  /* <video class="video" src="img/sweden.mp4"></video> */
+}
